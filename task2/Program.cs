ae7ec6716b278457bfad82fba78e4cccc02b811f
Anna <Ancha.ru@mail.ru>
2022-09-22 17:37:30 +0300

    second task

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.*/
+Console.Clear();
+Random rnd = new Random();
+int number= rnd.Next();
+Console.WriteLine(number);
+string strnum = Convert.ToString(number);
+if (strnum.Length > 2)
+  Console.WriteLine("Третья цифра: " + strnum[2]);
+else 
+  Console.WriteLine("Третьей цифры нет");
